297c107bf8e8490d8bc9b42b850325868d8ec8a4
Emanuelle <emanuellecortezc@gmail.com>
2024-08-31 19:06:09 -0300

    feat(cache): inserido cache de 20 minutos na api de consulta de cliente

--- app/Http/Controllers/Api/ClientsController.php
+++ app/Http/Controllers/Api/ClientsController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Resources\ClientResource;
 use App\Models\Clients;
+use Illuminate\Support\Facades\Cache;
 
 
 class ClientsController extends Controller
@@ -15,13 +16,16 @@ class ClientsController extends Controller
      */
     public function index(Request $request)
     {
-        $clients = Clients::query();
+        // Verificar se o resultado já está no cache, caso contrário cria uma chave de hash
+        $clients = Cache::remember('clients_' . hash('sha256', json_encode($request->all())), 1200, function () use ($request) {
+            $query = Clients::query();
 
-        if ($request->has('name')) {
-            $clients->where('name', 'like', '%' . $request->input('name') . '%');
-        }
+            if ($request->has('name')) {
+                $query->where('name', 'like', '%' . $request->input('name') . '%');
+            }
 
-        $clients = $clients->with('phones', 'sellers', 'type')->paginate(15);
+            return $query->with('phones', 'sellers', 'type')->paginate(15);
+        });
 
         return ClientResource::collection($clients);
     }
